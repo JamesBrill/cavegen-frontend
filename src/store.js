35041c8d680bc90791d9b660577cf889b380bd57
James Brill <jebssnooker@hotmail.com>
2016-11-20 15:05:59 +0000

    Stop persisting authentication for now

--- src/store.js
+++ src/store.js
@@ -1,13 +1,11 @@
 import { applyMiddleware, compose, createStore, combineReducers } from 'redux'
 import { routerReducer, routerMiddleware } from 'react-router-redux'
 import { hashHistory } from 'react-router'
-import persistState from 'redux-localstorage'
 import thunkMiddleware from 'redux-thunk'
 import analyticsMiddleware from 'src/middleware/analytics'
 import * as reducers from 'src/reducers'
 
 const store = compose(
-  persistState('authentication'),
   applyMiddleware(
     analyticsMiddleware,
     thunkMiddleware,
