72c6eb769650359c7ea94d56df1bac3dfc63506b
James Brill <jebssnooker@hotmail.com>
2016-11-20 19:35:59 +0000

    Migrate cave storage util over

---
+++ src/editor/utils/cave-storage.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-invalid-this */
+
+import caveViewModel from '' // TODO: make appropriate import here
+
+export class CaveStorage {
+  constructor() {
+    this.caveNames = this.loadAllCaveNames()
+    this.selectedCaveName = ''
+  }
+
+  loadCave = function (caveName) {
+    if (!caveName || caveName === '') {
+      return
+    }
+    const caveString = localStorage[`cavegen_${caveName}`]
+    // _gaq.push(['_trackEvent', 'Storage', 'Load Cave', caveViewModel.caveName(), caveViewModel.caveWidth() * caveViewModel.caveHeight()])
+    caveViewModel.loadCave(caveName, caveString)
+  }
+
+  loadAllCaveNames = function () {
+    const caveNames = []
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i)
+      if (key.indexOf('cavegen_') === 0) {
+        caveNames.push(key.substring(8))
+      }
+    }
+    return caveNames
+  }
+
+  storeCave = function () {
+    const caveName = caveViewModel.caveName()
+    const caveString = caveViewModel.getCaveString()
+    let addToCaveNameList = false
+    if (!localStorage[`cavegen_${caveName}`]) {
+      addToCaveNameList = true
+    }
+
+    localStorage[`cavegen_${caveName}`] = caveString
+    // _gaq.push(['_trackEvent', 'Storage', 'Save Cave', caveViewModel.caveName(), caveViewModel.caveWidth() * caveViewModel.caveHeight()])
+
+    // The flag seems redundant, but need to make sure storage did not fail before
+    // adding the name to the list
+    if (addToCaveNameList) {
+      this.caveNames.push(caveName)
+    }
+  }
+}
