1bcdf96ea7d13c7d4d91a6c8b15d86093a2d64d5
James Brill <jebssnooker@hotmail.com>
2016-12-28 18:20:22 +0000

    Add login URLs to config

--- src/config/development.js
+++ src/config/development.js
@@ -1,1 +1,3 @@
 export const API_ROOT = 'http://192.168.31.12:8000/api'
+
+export const LOGIN_URL = 'http://localhost:3000/login'
--- src/config/production.js
+++ src/config/production.js
@@ -1,1 +1,3 @@
 export const API_ROOT = 'http://cavegen-django-env.cpkepspdmm.eu-west-1.elasticbeanstalk.com/api'
+
+export const LOGIN_URL = '' // TODO: set up production login URL
