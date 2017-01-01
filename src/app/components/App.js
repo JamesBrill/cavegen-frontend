d3d6146ecf4baabc01739d015e9dff32929a79c6
James Brill <jebssnooker@hotmail.com>
2017-01-01 21:51:05 +0000

    Remove react-dnd

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -1,10 +1,7 @@
 import React, { PureComponent, PropTypes, Children, cloneElement } from 'react'
-import { DragDropContext } from 'react-dnd'
-import HTML5Backend from 'react-dnd-html5-backend'
 
 import styles from 'src/app/components/App.css'
 
-@DragDropContext(HTML5Backend)
 export default class App extends PureComponent {
   static propTypes = {
     children: PropTypes.node
