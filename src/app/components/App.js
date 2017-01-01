26577c058695a4c431efe1330e8df1518d6cfc2d
James Brill <jebssnooker@hotmail.com>
2017-01-01 21:56:08 +0000

    Log out when JWT expires

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -1,15 +1,31 @@
 import React, { PureComponent, PropTypes, Children, cloneElement } from 'react'
+import { logout } from 'src/authentication/actions'
+import { connect } from 'react-redux'
+import moment from 'moment'
+import ScheduledEvent from 'src/utils/ScheduledEvent'
 
 import styles from 'src/app/components/App.css'
 
+function mapStateToProps(state) {
+  return {
+    tokenExpiryTime: state.authentication.claims.exp
+  }
+}
+
+@connect(mapStateToProps, { logout })
 export default class App extends PureComponent {
   static propTypes = {
-    children: PropTypes.node
+    children: PropTypes.node,
+    tokenExpiryTime: PropTypes.number,
+    logout: PropTypes.func
   };
 
   render() {
+    const { logout, tokenExpiryTime } = this.props // eslint-disable-line no-shadow
+
     return (
       <div className={styles.App}>
+        <ScheduledEvent when={moment.unix(tokenExpiryTime)} action={logout} />
         {cloneElement(Children.only(this.props.children), { className: styles.page })}
       </div>
     )
---
+++ src/utils/ScheduledEvent.js
@@ -0,0 +1,51 @@
+import { PureComponent, PropTypes } from 'react'
+import moment from 'moment'
+
+export default class ScheduledEvent extends PureComponent {
+  static propTypes = {
+    when: PropTypes.instanceOf(moment),
+    action: PropTypes.func
+  };
+
+  componentDidMount() {
+    this.updateScheduledAction()
+  }
+
+  componentDidUpdate(prevProps) {
+    const oldWhen = prevProps.when
+    const newWhen = this.props.when
+
+    const hasChanged = !moment.isMoment(newWhen) || !newWhen.isSame(oldWhen)
+
+    if (hasChanged) {
+      this.updateScheduledAction()
+    }
+  }
+
+  componentWillUnmount() {
+    this.clearScheduledAction()
+  }
+
+  updateScheduledAction() {
+    const { when, action } = this.props
+    const now = moment()
+
+    this.clearScheduledAction()
+
+    if (moment.isMoment(when)) {
+      const millisecondsFromNow = when.diff(now)
+
+      this.timeoutHandle = setTimeout(action, millisecondsFromNow)
+    }
+  }
+
+  clearScheduledAction() {
+    if (this.timeoutHandle) {
+      clearTimeout(this.timeoutHandle)
+    }
+  }
+
+  render() {
+    return null
+  }
+}
