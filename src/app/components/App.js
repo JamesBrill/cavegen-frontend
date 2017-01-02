5e91b69fccc478ef3f862922c37f05e66fbe205a
James Brill <jebssnooker@hotmail.com>
2017-01-02 12:04:04 +0000

    Only load tile images on editor page

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -2,22 +2,28 @@ import React, { PureComponent, PropTypes, Children, cloneElement } from 'react'
 import { logout } from 'src/authentication/actions'
 import { loadImages } from 'src/editor/actions'
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router'
 import moment from 'moment'
 import ScheduledEvent from 'src/utils/ScheduledEvent'
 
 import styles from 'src/app/components/App.css'
 
 function mapStateToProps(state) {
+  const location = state.routing.locationBeforeTransitions
+  const isEditorOpen = location.pathname.startsWith('/editor')
   return {
-    tokenExpiryTime: state.authentication.claims.exp
+    tokenExpiryTime: state.authentication.claims.exp,
+    isEditorOpen
   }
 }
 
 @connect(mapStateToProps, { logout, loadImages })
+@withRouter
 export default class App extends PureComponent {
   static propTypes = {
     children: PropTypes.node,
     tokenExpiryTime: PropTypes.number,
+    isEditorOpen: PropTypes.bool,
     logout: PropTypes.func,
     loadImages: PropTypes.func
   };
@@ -31,13 +37,23 @@ export default class App extends PureComponent {
   }
 
   componentWillMount() {
-    this.props.loadImages().then(() => this.setState({ imagesLoaded: true }))
+    const { isEditorOpen, loadImages } = this.props // eslint-disable-line no-shadow
+    if (isEditorOpen) {
+      loadImages().then(() => this.setState({ imagesLoaded: true }))
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { isEditorOpen, loadImages } = nextProps // eslint-disable-line no-shadow
+    if (isEditorOpen) {
+      loadImages().then(() => this.setState({ imagesLoaded: true }))
+    }
   }
 
   render() {
-    const { logout, tokenExpiryTime } = this.props // eslint-disable-line no-shadow
+    const { logout, tokenExpiryTime, isEditorOpen } = this.props // eslint-disable-line no-shadow
 
-    if (!this.state.imagesLoaded) {
+    if (isEditorOpen && !this.state.imagesLoaded) {
       return null
     }
 
