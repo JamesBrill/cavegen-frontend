dbb52f8a83112f9687b6f911cb805ef1012aa6bc
James Brill <jebssnooker@hotmail.com>
2017-01-14 21:40:04 +0000

    Extract EditorControls component

---
+++ src/editor/components/EditorControls.jsx
@@ -0,0 +1,93 @@
+import React, { PureComponent, PropTypes } from 'react'
+import classNames from 'classnames'
+import { connect } from 'react-redux'
+import { autobind } from 'core-decorators'
+import keydown from 'react-keydown'
+import Button from 'src/components/Button'
+import BrushSizeSelector from 'src/editor/components/BrushSizeSelector'
+import CopyToClipboard from 'src/editor/components/CopyToClipboard'
+import Play from 'src/editor/components/icons/Play'
+import Undo from 'src/editor/components/icons/Undo'
+import Redo from 'src/editor/components/icons/Redo'
+import Palette from 'src/editor/components/Palette'
+import ReactTooltip from 'react-tooltip'
+
+import styles from 'src/editor/components/EditorControls.css'
+
+import {
+  setCurrentBrush,
+  setBrushSize,
+  undoCaveChange,
+  redoCaveChange,
+  playCave
+} from 'src/editor/actions'
+
+function mapStateToProps(state) {
+  return {
+    caveCode: state.editor.caveCode
+  }
+}
+
+const mapDispatchToProps = {
+  dispatchSetCurrentBrush: setCurrentBrush,
+  dispatchSetBrushSize: setBrushSize,
+  dispatchUndo: undoCaveChange,
+  dispatchRedo: redoCaveChange,
+  dispatchPlayCave: playCave
+}
+
+const UNDO_KEYS = ['ctrl+z', 'cmd+z']
+const REDO_KEYS = ['ctrl+y', 'cmd+y', 'ctrl+shift+z', 'cmd+shift+z']
+
+@connect(mapStateToProps, mapDispatchToProps)
+export default class EditorControls extends PureComponent {
+  static propTypes = {
+    className: PropTypes.string,
+    caveCode: PropTypes.string,
+    dispatchSetCurrentBrush: PropTypes.func,
+    dispatchSetBrushSize: PropTypes.func,
+    dispatchUndo: PropTypes.func,
+    dispatchRedo: PropTypes.func,
+    dispatchPlayCave: PropTypes.func
+  };
+
+  @autobind
+  @keydown(UNDO_KEYS)
+  handleUndo(e) {
+    e.preventDefault()
+    this.props.dispatchUndo()
+  }
+
+  @autobind
+  @keydown(REDO_KEYS)
+  handleRedo(e) {
+    e.preventDefault()
+    this.props.dispatchRedo()
+  }
+
+  render() {
+    const { className, caveCode, dispatchSetBrushSize, dispatchPlayCave,
+            dispatchSetCurrentBrush } = this.props
+    const computedClassName = classNames(styles.EditorControls, className)
+
+    return (
+      <div className={computedClassName}>
+        <BrushSizeSelector className={styles.brushSize} onBrushSizeChange={dispatchSetBrushSize} />
+        <div className={styles.buttonContainer}>
+          <CopyToClipboard caveCode={caveCode} data-tip='Undo' />
+          <Button className={styles.iconButton} onClick={this.handleUndo} data-tip='Undo'>
+            <Undo className={styles.icon} />
+          </Button>
+          <Button className={styles.iconButton} onClick={this.handleRedo} data-tip='Redo'>
+            <Redo className={styles.icon} />
+          </Button>
+          <Button className={styles.iconButton} onClick={dispatchPlayCave} data-tip='Play'>
+            <Play className={styles.icon} />
+          </Button>
+          <ReactTooltip effect='solid' delayShow={250} />
+        </div>
+        <Palette onTileClick={dispatchSetCurrentBrush} />
+      </div>
+    )
+  }
+}
--- src/editor/components/EditorPage.jsx
+++ src/editor/components/EditorPage.jsx
@@ -3,32 +3,21 @@ import classNames from 'classnames'
 import { connect } from 'react-redux'
 import keydown from 'react-keydown'
 import { autobind } from 'core-decorators'
-import Palette from 'src/editor/components/Palette'
 import Grid from 'src/editor/components/Grid'
 import { getTileFromSymbol, TILE_KEYS } from 'src/editor/utils/tiles'
-import BrushSizeSelector from 'src/editor/components/BrushSizeSelector'
-import CopyToClipboard from 'src/editor/components/CopyToClipboard'
 import EditorBanner from 'src/editor/components/EditorBanner'
-import Button from 'src/components/Button'
+import EditorControls from 'src/editor/components/EditorControls'
 import requiresAuthentication from 'src/authentication/utils/requiresAuthentication'
 import { Cave } from 'src/editor/utils/cave'
-import Play from 'src/editor/components/icons/Play'
-import Undo from 'src/editor/components/icons/Undo'
-import Redo from 'src/editor/components/icons/Redo'
-import ReactTooltip from 'react-tooltip'
 
 import styles from 'src/editor/components/EditorPage.css'
 
 import {
   setGrid,
   setCurrentBrush,
-  setBrushSize,
   setCaveWidth,
   setCaveHeight,
-  startRebuild,
-  undoCaveChange,
-  redoCaveChange,
-  playCave
+  startRebuild
 } from 'src/editor/actions'
 import {
   loadCaves,
@@ -40,7 +29,6 @@ function mapStateToProps(state) {
   return {
     caveWidth: state.editor.caveWidth,
     caveHeight: state.editor.caveHeight,
-    caveCode: state.editor.caveCode,
     caves: state.caves.caves
   }
 }
@@ -48,20 +36,13 @@ function mapStateToProps(state) {
 const mapDispatchToProps = {
   dispatchSetGrid: setGrid,
   dispatchSetCurrentBrush: setCurrentBrush,
-  dispatchSetBrushSize: setBrushSize,
   dispatchSetCaveWidth: setCaveWidth,
   dispatchSetCaveHeight: setCaveHeight,
   dispatchStartRebuild: startRebuild,
-  dispatchUndo: undoCaveChange,
-  dispatchRedo: redoCaveChange,
-  dispatchPlayCave: playCave,
   dispatchLoadCaves: loadCaves,
   dispatchLoadPublicCaves: loadPublicCaves,
   dispatchLoadCaveIntoGrid: loadCaveIntoGrid
 }
-
-const UNDO_KEYS = ['ctrl+z', 'cmd+z']
-const REDO_KEYS = ['ctrl+y', 'cmd+y', 'ctrl+shift+z', 'cmd+shift+z']
 
 @connect(mapStateToProps, mapDispatchToProps)
 @requiresAuthentication
@@ -73,13 +54,9 @@ export default class EditorPage extends PureComponent {
     caves: PropTypes.arrayOf(PropTypes.object),
     dispatchSetGrid: PropTypes.func,
     dispatchSetCurrentBrush: PropTypes.func,
-    dispatchSetBrushSize: PropTypes.func,
     dispatchSetCaveWidth: PropTypes.func,
     dispatchSetCaveHeight: PropTypes.func,
     dispatchStartRebuild: PropTypes.func,
-    dispatchUndo: PropTypes.func,
-    dispatchRedo: PropTypes.func,
-    dispatchPlayCave: PropTypes.func,
     dispatchLoadCaves: PropTypes.func,
     dispatchLoadPublicCaves: PropTypes.func,
     dispatchLoadCaveIntoGrid: PropTypes.func
@@ -116,20 +93,6 @@ export default class EditorPage extends PureComponent {
   }
 
   @autobind
-  @keydown(UNDO_KEYS)
-  handleUndo(e) {
-    e.preventDefault()
-    this.props.dispatchUndo()
-  }
-
-  @autobind
-  @keydown(REDO_KEYS)
-  handleRedo(e) {
-    e.preventDefault()
-    this.props.dispatchRedo()
-  }
-
-  @autobind
   @keydown(TILE_KEYS)
   handleTileKeyPress(e) {
     let tile
@@ -153,31 +116,14 @@ export default class EditorPage extends PureComponent {
   }
 
   render() {
-    const { className, caveCode, dispatchSetCurrentBrush,
-            dispatchSetBrushSize, dispatchPlayCave } = this.props
+    const { className } = this.props
     const computedClassName = classNames(styles.EditorPage, className)
 
     return (
       <div className={computedClassName}>
         <EditorBanner onCaveRebuild={this.handleRebuild} />
         <div className={styles.editor}>
-          <div className={styles.editorControls}>
-            <BrushSizeSelector className={styles.brushSize} onBrushSizeChange={dispatchSetBrushSize} />
-            <div className={styles.buttonContainer}>
-              <CopyToClipboard caveCode={caveCode} data-tip='Undo' />
-              <Button className={styles.iconButton} onClick={this.handleUndo} data-tip='Undo'>
-                <Undo className={styles.icon} />
-              </Button>
-              <Button className={styles.iconButton} onClick={this.handleRedo} data-tip='Redo'>
-                <Redo className={styles.icon} />
-              </Button>
-              <Button className={styles.iconButton} onClick={dispatchPlayCave} data-tip='Play'>
-                <Play className={styles.icon} />
-              </Button>
-              <ReactTooltip effect='solid' delayShow={250} />
-            </div>
-            <Palette onTileClick={dispatchSetCurrentBrush} />
-          </div>
+          <EditorControls className={styles.editorControls} />
           <Grid className={styles.grid} ref={grid => (this.grid = grid)} />
         </div>
       </div>
