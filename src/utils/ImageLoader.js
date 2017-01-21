fbc1d49450d5c159ea7c91a50fd7c94ab4669fe5
James Brill <jebssnooker@hotmail.com>
2017-01-21 15:24:03 +0000

    Add hotkey symbols to tooltips

--- src/utils/ImageLoader.js
+++ src/utils/ImageLoader.js
@@ -1,33 +1,33 @@
 export const PALETTE_BRUSHES = {
   Tools: [
-    { fileName: 'space', symbol: ' ', tooltip: 'Eraser' },
-    { fileName: 'spikefiller', symbol: 'f', tooltip: 'Spike Filler' },
-    { fileName: 'spikeremover', symbol: 'r', tooltip: 'Spike Remover' }
+    { fileName: 'space', symbol: ' ', tooltip: 'Eraser s' },
+    { fileName: 'spikefiller', symbol: 'f', tooltip: 'Spike Filler f' },
+    { fileName: 'spikeremover', symbol: 'r', tooltip: 'Spike Remover r' }
   ],
   Common: [
-    { fileName: 'terrain', symbol: 'x', tooltip: 'Terrain' },
-    { fileName: 'treasure', symbol: '+', tooltip: 'Treasure' },
-    { fileName: 'hannah', symbol: '#', tooltip: 'Starting Position' },
-    { fileName: 'door', symbol: 'D', tooltip: 'Level Exit' },
-    { fileName: 'woodencrate', symbol: 'b', tooltip: 'Wooden Crate' },
-    { fileName: 'steelcrate', symbol: 'k', tooltip: 'Steel Crate' },
-    { fileName: 'boulder', symbol: 'o', tooltip: 'Boulder' },
-    { fileName: 'upspike', symbol: 'm', tooltip: 'Stalagmite' },
-    { fileName: 'downspike', symbol: 'w', tooltip: 'Stalactite' },
-    { fileName: 'platform', symbol: '=', tooltip: 'Platform' },
-    { fileName: 'ladder', symbol: '\"', tooltip: 'Ladder' }
+    { fileName: 'terrain', symbol: 'x', tooltip: 'Terrain x' },
+    { fileName: 'treasure', symbol: '+', tooltip: 'Treasure +' },
+    { fileName: 'hannah', symbol: '#', tooltip: 'Starting Position #' },
+    { fileName: 'door', symbol: 'D', tooltip: 'Level Exit d' },
+    { fileName: 'woodencrate', symbol: 'b', tooltip: 'Wooden Crate b' },
+    { fileName: 'steelcrate', symbol: 'k', tooltip: 'Steel Crate k' },
+    { fileName: 'boulder', symbol: 'o', tooltip: 'Boulder o' },
+    { fileName: 'upspike', symbol: 'm', tooltip: 'Stalagmite m' },
+    { fileName: 'downspike', symbol: 'w', tooltip: 'Stalactite w' },
+    { fileName: 'platform', symbol: '=', tooltip: 'Platform =' },
+    { fileName: 'ladder', symbol: '\"', tooltip: 'Ladder \"' }
   ],
   Destructive: [
-    { fileName: 'arrowup', symbol: '^', tooltip: 'Arrow Up' },
-    { fileName: 'arrowleft', symbol: '<', tooltip: 'Arrow Left' },
-    { fileName: 'arrowdown', symbol: 'v', tooltip: 'Arrow Down' },
-    { fileName: 'arrowright', symbol: '>', tooltip: 'Arrow Right' },
-    { fileName: 'steelarrowup', symbol: '\'', tooltip: 'Steel Arrow Up' },
-    { fileName: 'steelarrowleft', symbol: '{', tooltip: 'Steel Arrow Left' },
-    { fileName: 'steelarrowdown', symbol: ',', tooltip: 'Steel Arrow Down' },
-    { fileName: 'steelarrowright', symbol: '}', tooltip: 'Steel Arrow Right' },
-    { fileName: 'dynamite', symbol: '/', tooltip: 'Wooden Dynamite' },
-    { fileName: 'steeldynamite', symbol: '|', tooltip: 'Steel Dynamite' }
+    { fileName: 'arrowup', symbol: '^', tooltip: 'Arrow Up ^' },
+    { fileName: 'arrowleft', symbol: '<', tooltip: 'Arrow Left <' },
+    { fileName: 'arrowdown', symbol: 'v', tooltip: 'Arrow Down v' },
+    { fileName: 'arrowright', symbol: '>', tooltip: 'Arrow Right >' },
+    { fileName: 'steelarrowup', symbol: '\'', tooltip: 'Steel Arrow Up \'' },
+    { fileName: 'steelarrowleft', symbol: '{', tooltip: 'Steel Arrow Left {' },
+    { fileName: 'steelarrowdown', symbol: ',', tooltip: 'Steel Arrow Down ,' },
+    { fileName: 'steelarrowright', symbol: '}', tooltip: 'Steel Arrow Right }' },
+    { fileName: 'dynamite', symbol: '/', tooltip: 'Wooden Dynamite /' },
+    { fileName: 'steeldynamite', symbol: '|', tooltip: 'Steel Dynamite |' }
   ],
   Enemies: [
     { fileName: 'enemy1', symbol: '1', tooltip: 'Enemy 1' },
@@ -37,21 +37,21 @@ export const PALETTE_BRUSHES = {
     { fileName: 'enemy5', symbol: '5', tooltip: 'Enemy 5' }
   ],
   Water: [
-    { fileName: 'watercrate', symbol: '@', tooltip: 'Water Crate' },
-    { fileName: 'watertap', symbol: '!', tooltip: 'Water Tap Crate' },
-    { fileName: 'waterlevel', symbol: '~', tooltip: 'Water Surface' },
-    { fileName: 'waterstart', symbol: 't', tooltip: 'Water Source' },
-    { fileName: 'airpocket', symbol: '.', tooltip: 'Air Pocket' }
+    { fileName: 'watercrate', symbol: '@', tooltip: 'Water Crate @' },
+    { fileName: 'watertap', symbol: '!', tooltip: 'Water Tap Crate !' },
+    { fileName: 'waterlevel', symbol: '~', tooltip: 'Water Surface ~' },
+    { fileName: 'waterstart', symbol: 't', tooltip: 'Water Source t' },
+    { fileName: 'airpocket', symbol: '.', tooltip: 'Air Pocket .' }
   ],
   Rare: [
-    { fileName: 'tutorialup', symbol: 'n', tooltip: 'Pointer Up' },
-    { fileName: 'tutorialleft', symbol: '(', tooltip: 'Pointer Left' },
-    { fileName: 'tutorialdown', symbol: 'u', tooltip: 'Pointer Down' },
-    { fileName: 'tutorialright', symbol: ')', tooltip: 'Pointer Right' },
-    { fileName: 'secret', symbol: 'z', tooltip: 'Secret Area' },
-    { fileName: 'heart', symbol: 'l', tooltip: 'Heart' },
-    { fileName: 'gem', symbol: 'g', tooltip: 'Gem' },
-    { fileName: 'clone', symbol: 'c', tooltip: 'Fake Crate' }
+    { fileName: 'tutorialup', symbol: 'n', tooltip: 'Pointer Up n' },
+    { fileName: 'tutorialleft', symbol: '(', tooltip: 'Pointer Left (' },
+    { fileName: 'tutorialdown', symbol: 'u', tooltip: 'Pointer Down u' },
+    { fileName: 'tutorialright', symbol: ')', tooltip: 'Pointer Right )' },
+    { fileName: 'secret', symbol: 'z', tooltip: 'Secret Area z' },
+    { fileName: 'heart', symbol: 'l', tooltip: 'Heart l' },
+    { fileName: 'gem', symbol: 'g', tooltip: 'Gem g' },
+    { fileName: 'clone', symbol: 'c', tooltip: 'Fake Crate c' }
   ]
 }
 const ASSETS_PATH = 'static'
