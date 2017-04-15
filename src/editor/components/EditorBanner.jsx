4b5eb6f96af71ff9096c45c3898873c66949d94a
James Brill <jebssnooker@hotmail.com>
2017-04-15 12:25:42 +0100

    Remove cave lists from navbar

--- src/editor/components/EditorBanner.jsx
+++ src/editor/components/EditorBanner.jsx
@@ -3,8 +3,6 @@ import classNames from 'classnames'
 import { connect } from 'react-redux'
 import { autobind } from 'core-decorators'
 import Button from 'src/components/Button'
-import Select from 'react-select'
-import 'react-select/dist/react-select.css'
 import HotkeysModal from 'src/editor/components/HotkeysModal'
 import NewCaveModal from 'src/editor/components/NewCaveModal'
 import ProfileModal from 'src/profile/components/ProfileModal'
@@ -21,8 +19,6 @@ import styles from 'src/editor/components/EditorBanner.css'
 
 function mapStateToProps(state) {
   return {
-    caves: state.caves.caves,
-    publicCaves: state.caves.publicCaves,
     userId: state.profile.userId,
     openTab: state.editor.openTab,
     isOwnedByAnotherUser: state.caves.isOwnedByAnotherUser
@@ -34,46 +30,11 @@ export default class EditorBanner extends PureComponent {
   static propTypes = {
     className: PropTypes.string,
     dispatch: PropTypes.func,
-    caves: PropTypes.arrayOf(PropTypes.object),
-    publicCaves: PropTypes.arrayOf(PropTypes.object),
     onCaveRebuild: PropTypes.func.isRequired,
     userId: PropTypes.number,
     openTab: PropTypes.oneOf(['palette', 'properties']),
     isOwnedByAnotherUser: PropTypes.bool
   };
-
-  getCaveSelectOptions(cave) {
-    return {
-      value: cave.id,
-      label: cave.name
-    }
-  }
-
-  getPublicCaveSelectOptions(cave) {
-    return {
-      value: cave.id,
-      label: `${cave.name} (${cave.authorName})`
-    }
-  }
-
-  @autobind
-  handleUserCaveChange(value) {
-    const { caves, dispatch } = this.props
-    const cave = caves.filter(c => c.id === value)[0]
-    dispatch(loadCaveIntoGrid(cave))
-  }
-
-  @autobind
-  handlePublicCaveChange(value) {
-    const { publicCaves, caves, dispatch, userId } = this.props
-    const cave = publicCaves.filter(c => c.id === value)[0]
-    if (userId === cave.author) {
-      const userCave = caves.filter(c => c.id === value)[0]
-      dispatch(loadCaveIntoGrid(userCave))
-    } else {
-      dispatch(loadCaveIntoGrid(cave))
-    }
-  }
 
   @autobind
   handlePropertiesClick() {
@@ -82,11 +43,8 @@ export default class EditorBanner extends PureComponent {
   }
 
   render() {
-    const { className, caves, publicCaves, openTab, isOwnedByAnotherUser } = this.props
+    const { className, openTab, isOwnedByAnotherUser } = this.props
     const computedClassName = classNames(styles.EditorBanner, className)
-    // This is a bit cheeky as it reorders an array in the Redux store
-    const userCaveOptions = caves.sort((a, b) => a.id - b.id).map(this.getCaveSelectOptions)
-    const publicCaveOptions = publicCaves.sort((a, b) => a.id - b.id).map(this.getPublicCaveSelectOptions)
     const tabText = (!isOwnedByAnotherUser && openTab === 'properties') ? 'Palette' : 'Properties'
 
     return (
@@ -94,18 +52,6 @@ export default class EditorBanner extends PureComponent {
         <div className={styles.left}>
           <NewCaveModal onCaveRebuild={this.props.onCaveRebuild} />
           <Button className={styles.button} onClick={this.handlePropertiesClick}>{tabText}</Button>
-          <div className={styles.myCaves}>
-            <Select
-              options={userCaveOptions}
-              placeholder='My Caves'
-              onChange={this.handleUserCaveChange} />
-          </div>
-          <div className={styles.myCaves}>
-            <Select
-              options={publicCaveOptions}
-              placeholder='Public Caves'
-              onChange={this.handlePublicCaveChange} />
-          </div>
         </div>
         <div className={styles.right}>
           <HotkeysModal />
