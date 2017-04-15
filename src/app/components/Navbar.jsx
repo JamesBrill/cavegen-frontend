d6a03400a6b84e6afb854beca8e88f89d07e469b
James Brill <jebssnooker@hotmail.com>
2017-04-15 20:40:29 +0100

    Dummy tabs in navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { PureComponent, PropTypes } from 'react'
+import { Link } from 'react-router'
 import classNames from 'classnames'
 
 import styles from 'src/app/components/Navbar.css'
@@ -19,7 +20,26 @@ export default class Navbar extends PureComponent {
 
         </div>
         <div className={styles.right}>
-
+          <Link to='/play'>
+            <div className={styles.tab}>
+              <p>Play</p>
+            </div>
+          </Link>
+          <Link to='/build'>
+            <div className={styles.tab}>
+              <p>Editor</p>
+            </div>
+          </Link>
+          <Link to='/learn'>
+            <div className={styles.tab}>
+              <p>Learn</p>
+            </div>
+          </Link>
+          <Link to='/profile'>
+            <div className={styles.tab}>
+              <p>Profile</p>
+            </div>
+          </Link>
         </div>
       </div>
     )
