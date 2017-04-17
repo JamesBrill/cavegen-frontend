30bf64d650af497dcd3c6ec6413fb91ad35fcac5
James Brill <jebssnooker@hotmail.com>
2017-04-17 19:49:37 +0100

    Make liking and name elements of level page dependent on whether cave belongs to user or not

--- src/levels/components/LevelPage.jsx
+++ src/levels/components/LevelPage.jsx
@@ -1,12 +1,19 @@
+/* eslint-disable no-shadow */
+
 import React, { PureComponent, PropTypes } from 'react'
 import { connect } from 'react-redux'
+import { autobind } from 'core-decorators'
 import classNames from 'classnames'
 import moment from 'moment'
+import Input from 'src/components/Input'
 import Like from 'src/editor/components/icons/Like'
 import requiresAuthentication from 'src/authentication/utils/requiresAuthentication'
 import withNavbar from 'src/app/utils/withNavbar'
 
 import styles from 'src/levels/components/LevelPage.css'
+
+import { likeCave } from 'src/levels/actions'
+import { updateCave } from 'src/editor/actions'
 
 function mapStateToProps(state, ownProps) {
   const levelId = parseInt(ownProps.params.id, 10)
@@ -14,34 +21,72 @@ function mapStateToProps(state, ownProps) {
   const level = levels.find(x => x.id === levelId)
   const userId = state.profile.userId
   const isOwnedByUser = userId === level.author
+  const isLikedByUser = state.profile.likedCaves.indexOf(levelId) !== -1
   return {
+    name: level.name,
+    uuid: level.uuid,
     author: level.authorName,
     dateCreated: level.dateCreated,
     likes: level.likes,
-    isOwnedByUser
+    isOwnedByUser,
+    isLikedByUser
   }
 }
 
-@connect(mapStateToProps)
+@connect(mapStateToProps, { likeCave, updateCave })
 @requiresAuthentication
 @withNavbar
 export default class LevelPage extends PureComponent {
   static propTypes = {
     className: PropTypes.string,
     children: PropTypes.node,
+    name: PropTypes.string,
+    uuid: PropTypes.string,
     author: PropTypes.string,
     dateCreated: PropTypes.string,
     likes: PropTypes.number,
-    isOwnedByUser: PropTypes.bool
+    isOwnedByUser: PropTypes.bool,
+    isLikedByUser: PropTypes.bool,
+    likeCave: PropTypes.func,
+    updateCave: PropTypes.func
   };
 
+  @autobind
+  handleNameChange(e) {
+    const name = e.target.value
+    this.props.updateCave({ name })
+  }
+
   render() {
-    const { className, children, author, dateCreated, likes, isOwnedByUser } = this.props
+    const { className, children, name, uuid, author, dateCreated, likes,
+            isOwnedByUser, isLikedByUser, likeCave } = this.props
     const computedClassName = classNames(styles.LevelPage, className)
+    const nameField = isOwnedByUser ? (
+      <div className={styles.information}>
+        <h2 className={styles.title}>Name:</h2>
+        <Input
+          className={styles.propertyInput}
+          onChange={this.handleNameChange}
+          key={uuid}
+          defaultValue={name} />
+      </div>
+    ) : (
+      <div className={styles.information}>
+        <h2 className={styles.title}>Name:</h2>
+        <h2 className={styles.informationValue}>{name}</h2>
+      </div>
+    )
+    const likeColour = isLikedByUser ? 'red' : 'white'
+    const likeIcon = isOwnedByUser ? (
+      <Like className={styles.like} color='red' />
+    ) : (
+      <Like className={styles.like} color={likeColour} onClick={() => likeCave(uuid)} />
+    )
 
     return (
       <div className={computedClassName}>
         {children}
+        {nameField}
         <div className={styles.information}>
           <h2 className={styles.title}>Author:</h2>
           <h2 className={styles.informationValue}>{author}</h2>
@@ -53,7 +98,7 @@ export default class LevelPage extends PureComponent {
         <div className={styles.information}>
           <h2 className={styles.title}>Hearts:</h2>
           <div className={styles.likesContainer}>
-            <Like className={styles.like} color='red' />
+            {likeIcon}
             <h2 className={styles.informationValue}>{likes}</h2>
           </div>
         </div>
