896e0af90ca68dd32a0e36e3b9282094e0805be4
James Brill <jebssnooker@hotmail.com>
2017-04-17 15:06:55 +0100

    Show dummy list of levels in My Levels page

--- src/levels/components/MyLevelsPage.jsx
+++ src/levels/components/MyLevelsPage.jsx
@@ -1,26 +1,39 @@
 import React, { PureComponent, PropTypes } from 'react'
+import { connect } from 'react-redux'
 import classNames from 'classnames'
 import requiresAuthentication from 'src/authentication/utils/requiresAuthentication'
 import withNavbar from 'src/app/utils/withNavbar'
 
 import styles from 'src/levels/components/MyLevelsPage.css'
 
+function mapStateToProps(state) {
+  return {
+    myLevels: state.caves.caves
+  }
+}
+
+@connect(mapStateToProps)
 @requiresAuthentication
 @withNavbar
 export default class MyLevelsPage extends PureComponent {
   static propTypes = {
     className: PropTypes.string,
-    children: PropTypes.node
+    children: PropTypes.node,
+    myLevels: PropTypes.arrayOf(PropTypes.object)
   };
 
   render() {
-    const { className, children } = this.props
+    const { className, children, myLevels } = this.props
     const computedClassName = classNames(styles.MyLevelsPage, className)
 
     return (
       <div className={computedClassName}>
         {children}
-        <p>MyLevelsPage</p>
+        <div className={styles.levels}>
+          {myLevels.map(level => (
+            <p key={level.id}>{level.name}</p>
+          ))}
+        </div>
       </div>
     )
   }
