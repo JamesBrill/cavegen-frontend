cf0e4d75e0a4b3b20a0ebb22a0c2ec510dce284b
James Brill <jebssnooker@hotmail.com>
2017-04-17 16:02:31 +0100

    Make public cave list functional

--- src/levels/components/PublicLevelsPage.jsx
+++ src/levels/components/PublicLevelsPage.jsx
@@ -1,26 +1,42 @@
 import React, { PureComponent, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router'
 import classNames from 'classnames'
 import requiresAuthentication from 'src/authentication/utils/requiresAuthentication'
 import withNavbar from 'src/app/utils/withNavbar'
 
 import styles from 'src/levels/components/PublicLevelsPage.css'
 
+function mapStateToProps(state) {
+  return {
+    publicLevels: state.caves.publicCaves
+  }
+}
+
+@connect(mapStateToProps)
 @requiresAuthentication
 @withNavbar
 export default class PublicLevelsPage extends PureComponent {
   static propTypes = {
     className: PropTypes.string,
-    children: PropTypes.node
+    children: PropTypes.node,
+    publicLevels: PropTypes.arrayOf(PropTypes.object)
   };
 
   render() {
-    const { className, children } = this.props
+    const { className, children, publicLevels } = this.props
     const computedClassName = classNames(styles.PublicLevelsPage, className)
 
     return (
       <div className={computedClassName}>
         {children}
-        <p>PublicLevelsPage</p>
+        <div className={styles.levels}>
+          {publicLevels.map(level => (
+            <Link key={level.id} to={`/level/${level.id}`}>
+              <p>{level.name}</p>
+            </Link>
+          ))}
+        </div>
       </div>
     )
   }
