20a7fd3436a2483232b16cc7a4a87eaad3738f61
James Brill <jebssnooker@hotmail.com>
2017-04-17 16:01:04 +0100

    Show more detailed cave information on level page

--- src/levels/components/LevelPage.jsx
+++ src/levels/components/LevelPage.jsx
@@ -1,27 +1,43 @@
 import React, { PureComponent, PropTypes } from 'react'
+import { connect } from 'react-redux'
 import classNames from 'classnames'
 import requiresAuthentication from 'src/authentication/utils/requiresAuthentication'
 import withNavbar from 'src/app/utils/withNavbar'
 
 import styles from 'src/levels/components/LevelPage.css'
 
+function mapStateToProps(state, ownProps) {
+  const levelId = parseInt(ownProps.params.id, 10)
+  const levels = state.caves.caves.concat(state.caves.publicCaves)
+  const level = levels.find(x => x.id === levelId)
+  const userId = state.profile.userId
+  const isOwnedByUser = userId === level.author
+  return {
+    author: level.authorName,
+    isOwnedByUser
+  }
+}
+
+@connect(mapStateToProps)
 @requiresAuthentication
 @withNavbar
 export default class LevelPage extends PureComponent {
   static propTypes = {
     className: PropTypes.string,
     children: PropTypes.node,
-    params: PropTypes.shape({ id: PropTypes.string })
+    author: PropTypes.string,
+    isOwnedByUser: PropTypes.bool
   };
 
   render() {
-    const { className, children, params } = this.props
+    const { className, children, author, isOwnedByUser } = this.props
     const computedClassName = classNames(styles.LevelPage, className)
 
     return (
       <div className={computedClassName}>
         {children}
-        <p>{params.id}</p>
+        <p>{author}</p>
+        <p>Is this my cave? {isOwnedByUser ? 'yes' : 'no'}</p>
       </div>
     )
   }
