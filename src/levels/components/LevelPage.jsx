f3d00c177278c923fc7853d2e657fd282a5c8681
James Brill <jebssnooker@hotmail.com>
2017-04-17 19:27:27 +0100

    Add date created and hearts to level page

--- src/levels/components/LevelPage.jsx
+++ src/levels/components/LevelPage.jsx
@@ -1,6 +1,8 @@
 import React, { PureComponent, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import classNames from 'classnames'
+import moment from 'moment'
+import Like from 'src/editor/components/icons/Like'
 import requiresAuthentication from 'src/authentication/utils/requiresAuthentication'
 import withNavbar from 'src/app/utils/withNavbar'
 
@@ -14,6 +16,8 @@ function mapStateToProps(state, ownProps) {
   const isOwnedByUser = userId === level.author
   return {
     author: level.authorName,
+    dateCreated: level.dateCreated,
+    likes: level.likes,
     isOwnedByUser
   }
 }
@@ -26,18 +30,33 @@ export default class LevelPage extends PureComponent {
     className: PropTypes.string,
     children: PropTypes.node,
     author: PropTypes.string,
+    dateCreated: PropTypes.string,
+    likes: PropTypes.number,
     isOwnedByUser: PropTypes.bool
   };
 
   render() {
-    const { className, children, author, isOwnedByUser } = this.props
+    const { className, children, author, dateCreated, likes, isOwnedByUser } = this.props
     const computedClassName = classNames(styles.LevelPage, className)
 
     return (
       <div className={computedClassName}>
         {children}
-        <p>{author}</p>
-        <p>Is this my cave? {isOwnedByUser ? 'yes' : 'no'}</p>
+        <div className={styles.information}>
+          <h2 className={styles.title}>Author:</h2>
+          <h2 className={styles.informationValue}>{author}</h2>
+        </div>
+        <div className={styles.information}>
+          <h2 className={styles.title}>Date created:</h2>
+          <h2 className={styles.informationValue}>{moment(dateCreated).format('MM/DD/YYYY, h:mm a')}</h2>
+        </div>
+        <div className={styles.information}>
+          <h2 className={styles.title}>Hearts:</h2>
+          <div className={styles.likesContainer}>
+            <Like className={styles.like} color='red' />
+            <h2 className={styles.informationValue}>{likes}</h2>
+          </div>
+        </div>
       </div>
     )
   }
