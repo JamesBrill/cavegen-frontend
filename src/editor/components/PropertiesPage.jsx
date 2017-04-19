177620f9a1966c2f2bf1318bf91eec96a83bc780
James Brill <jebssnooker@hotmail.com>
2017-04-19 22:34:47 +0100

    Reuse CaveInformation in PropertiesPage

--- src/editor/components/PropertiesPage.jsx
+++ src/editor/components/PropertiesPage.jsx
@@ -1,26 +1,39 @@
 import React, { PureComponent, PropTypes } from 'react'
 import classNames from 'classnames'
+import { connect } from 'react-redux'
+import CaveInformation from 'src/levels/components/CaveInformation'
 import requiresAuthentication from 'src/authentication/utils/requiresAuthentication'
 import withNavbar from 'src/app/utils/withNavbar'
 
 import styles from 'src/editor/components/PropertiesPage.css'
 
+function mapStateToProps(state) {
+  const levels = state.levels.myLevels
+  const currentLevelUuid = state.editor.caveUuid
+  const currentLevel = levels.find(x => x.uuid === currentLevelUuid) || levels[0]
+  return {
+    levelId: currentLevel.id
+  }
+}
+
+@connect(mapStateToProps)
 @requiresAuthentication
 @withNavbar
 export default class PropertiesPage extends PureComponent {
   static propTypes = {
     className: PropTypes.string,
-    children: PropTypes.node
+    children: PropTypes.node,
+    levelId: PropTypes.number
   };
 
   render() {
-    const { className, children } = this.props
+    const { className, children, levelId } = this.props
     const computedClassName = classNames(styles.PropertiesPage, className)
 
     return (
       <div className={computedClassName}>
         {children}
-        <p>PropertiesPage</p>
+        <CaveInformation levelId={levelId} />
       </div>
     )
   }
