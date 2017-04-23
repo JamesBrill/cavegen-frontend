faf435696b351532aa697e2230c8c4054ac638f3
James Brill <jebssnooker@hotmail.com>
2017-04-23 15:01:06 +0100

    Move contents of profile modal into profile page

--- src/profile/components/ProfilePage.jsx
+++ src/profile/components/ProfilePage.jsx
@@ -1,26 +1,60 @@
 import React, { PureComponent, PropTypes } from 'react'
+import { browserHistory } from 'react-router'
 import classNames from 'classnames'
+import { autobind } from 'core-decorators'
+import { connect } from 'react-redux'
+import Button from 'src/components/Button'
+import Input from 'src/components/Input'
 import requiresAuthentication from 'src/authentication/utils/requiresAuthentication'
 import withNavbar from 'src/app/utils/withNavbar'
+import { logout } from 'src/authentication/actions'
+import { updateUserProfile } from 'src/profile/actions'
 
 import styles from 'src/profile/components/ProfilePage.css'
 
+function mapStateToProps(state) {
+  return {
+    displayName: state.profile.displayName
+  }
+}
+
+@connect(mapStateToProps, { logout, updateUserProfile })
 @requiresAuthentication
 @withNavbar
 export default class ProfilePage extends PureComponent {
   static propTypes = {
-    className: PropTypes.string,
-    children: PropTypes.node
+    logout: PropTypes.func,
+    updateUserProfile: PropTypes.func,
+    displayName: PropTypes.string
   };
 
+  @autobind
+  handleLogout() {
+    this.props.logout()
+    browserHistory.replace('/')
+  }
+
+  @autobind
+  handleDisplayNameChange(e) {
+    const displayName = e.target.value
+    this.props.updateUserProfile({ displayName })
+    this.setState({ publicLevelsNeedUpdating: true })
+  }
+
   render() {
-    const { className, children } = this.props
+    const { className, children, displayName } = this.props
     const computedClassName = classNames(styles.ProfilePage, className)
 
     return (
       <div className={computedClassName}>
         {children}
-        <p>ProfilePage</p>
+        <div className={styles.contents}>
+          <h2 className={styles.title}>Display Name</h2>
+          <Input onChange={this.handleDisplayNameChange} defaultValue={displayName} />
+          <div className={styles.buttons}>
+            <Button className={styles.logoutButton} onClick={this.handleLogout}>Logout</Button>
+          </div>
+        </div>
       </div>
     )
   }
