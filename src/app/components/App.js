cbd6549091f86dd5c935f45090d5b428290b3fe6
James Brill <contact@jamesbrill.co.uk>
2018-10-14 11:04:54 +0100

    Simplify App.jsx

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -4,16 +4,13 @@ import { connect } from 'react-redux'
 import { autobind } from 'core-decorators'
 import { withRouter } from 'react-router'
 import Spinner from 'src/components/Spinner'
-import { loadMyLevels, loadPublicLevels } from 'src/levels/actions'
+import { loadMyLevels } from 'src/levels/actions'
 import { loadCaveIntoGrid } from 'src/editor/actions'
 
 import styles from 'src/app/components/App.css'
 
 function mapStateToProps(state) {
-  const location = state.routing.locationBeforeTransitions
-  const isEditorOpen = !location.pathname.startsWith('/login')
   return {
-    isEditorOpen,
     levelsLoaded: state.levels.myLevels && state.levels.myLevels.length > 0
   }
 }
@@ -21,17 +18,14 @@ function mapStateToProps(state) {
 @connect(mapStateToProps, {
   loadImages,
   loadMyLevels,
-  loadPublicLevels,
   loadCaveIntoGrid
 })
 @withRouter
 export default class App extends PureComponent {
   static propTypes = {
     children: PropTypes.node,
-    isEditorOpen: PropTypes.bool,
     loadImages: PropTypes.func,
     loadMyLevels: PropTypes.func,
-    loadPublicLevels: PropTypes.func,
     loadCaveIntoGrid: PropTypes.func,
     caveUuid: PropTypes.string,
     levelsLoaded: PropTypes.bool
@@ -50,7 +44,6 @@ export default class App extends PureComponent {
       this.props.loadCaveIntoGrid()
     }
     this.props.loadMyLevels()
-    this.props.loadPublicLevels()
     this.loadImagesOrRedirectToLogin(this.props)
   }
 
@@ -63,16 +56,12 @@ export default class App extends PureComponent {
 
   @autobind
   loadImagesOrRedirectToLogin(props) {
-    const { isEditorOpen, loadImages } = props // eslint-disable-line no-shadow
-    if (isEditorOpen) {
-      loadImages().then(() => this.setState({ imagesLoaded: true }))
-    }
+    const { loadImages } = props // eslint-disable-line no-shadow
+    loadImages().then(() => this.setState({ imagesLoaded: true }))
   }
 
   render() {
-    const { isEditorOpen } = this.props // eslint-disable-line no-shadow
-
-    if (isEditorOpen && !this.state.imagesLoaded) {
+    if (!this.state.imagesLoaded) {
       return (
         <div className={styles.spinnerContainer}>
           <Spinner className={styles.spinner} />
